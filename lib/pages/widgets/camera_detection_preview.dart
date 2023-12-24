d9b7a198023f69efadcfaec269682a2678445507
habiboutlaw <138265029+habiboutlaw@users.noreply.github.com>
2023-12-24 16:57:47 +0200

    Add files via upload

---
+++ lib/pages/widgets/camera_detection_preview.dart
@@ -0,0 +1,60 @@
+import 'package:camera/camera.dart';
+import 'package:face_net_authentication/locator.dart';
+import 'package:face_net_authentication/pages/widgets/FacePainter.dart';
+import 'package:face_net_authentication/services/camera.service.dart';
+import 'package:face_net_authentication/services/face_detector_service.dart';
+import 'package:flutter/material.dart';
+
+class CameraDetectionPreview extends StatefulWidget {
+  CameraDetectionPreview({Key? key}) : super(key: key);
+
+  @override
+  State<CameraDetectionPreview> createState() => _CameraDetectionPreviewState();
+}
+
+class _CameraDetectionPreviewState extends State<CameraDetectionPreview> {
+   CameraService _cameraService = locator<CameraService>();
+
+   FaceDetectorService _faceDetectorService =
+      locator<FaceDetectorService>();
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Transform.scale(
+      scale: 1.0,
+      child: AspectRatio(
+        aspectRatio: MediaQuery.of(context).size.aspectRatio,
+        child: OverflowBox(
+          alignment: Alignment.center,
+          child: FittedBox(
+            fit: BoxFit.fitHeight,
+            child: Container(
+              width: width,
+              height:
+                  width * _cameraService.cameraController!.value.aspectRatio,
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  CameraPreview(_cameraService.cameraController!),
+                  if (_faceDetectorService.faceDetected)
+                    CustomPaint(
+                      painter: FacePainter(
+                        face: _faceDetectorService.faces[0],
+                        faceState: (value){
+                          setState(() {
+                            _faceDetectorService.faces[0]=value!;
+                          });
+                        },
+                        imageSize: _cameraService.getImageSize(),
+                      ),
+                    )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
